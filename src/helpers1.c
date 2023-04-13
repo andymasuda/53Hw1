c44924e4074f7e2f3e3dde82afb45e6ba5ad97c9
Andrew Masuda <masudaas@uci.edu>
2023-04-13 01:25:13 -0700

    updated helper functions

--- src/helpers1.c
+++ src/helpers1.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 void printNumLines(bool verbose) {
-  int numLines = 0;
+  int numLines = 1;
   int bufferLength = 255;
   char buffer[bufferLength];
 
@@ -21,34 +21,40 @@ void printNumLines(bool verbose) {
     }
     numLines++;
   }
-  printf("Total number of lines: %d\n", numLines);
+  printf("Total number of lines: %d\n", --numLines);
 }
 
 void printNumComments(bool verbose) {
+  int lineNum = 1;
   int numComments = 0;
   bool isComment = false;
   char c;
 
   for (c = getc(stdin); c != EOF; c = getc(stdin)) {
+    if (isComment && verbose) {
+      printf("%c", c);
+    }
     if (c == '#' && !isComment) {
-      ++numComments;
+      numComments++;
       isComment = true;
+      if (verbose) {
+        printf("%d: %c", lineNum, c);
+      }
     }
     if (c == '\n') {
       isComment = false;
+      lineNum++;
     }
   }
-
   printf("Total number of comments: %d\n", numComments);
 }
 
 bool isLabel(char *str) {
   int i = 0;
-  
+
   if (isdigit(str[i]) || isspace(str[i])) {
     return false;
   }
-
   while (isalnum(str[i]) || str[i] == '_' || str[i] == '.') {
     i++;
   }
@@ -60,12 +66,12 @@ bool isLabel(char *str) {
 
 void printNumLabels(bool verbose) {
   int numLabels = 0;
-  int numLines = 0;
+  int numLines = 1;
   int bufferLength = 255;
   char buffer[bufferLength];
 
-  while (fgets(buffer, bufferLength, stdin)) {  
-    if (isLabel(buffer)) {   
+  while (fgets(buffer, bufferLength, stdin)) {
+    if (isLabel(buffer)) {
       if (verbose) {
         fprintf(stderr, "%d: %s", numLines, buffer);
       }
@@ -73,14 +79,43 @@ void printNumLabels(bool verbose) {
     }
     numLines++;
   }
-
   printf("Total number of labels: %d\n", numLabels);
 }
 
 void printHistogram(char flag) {
-  printf("r %c\n", flag);
+  char c;
+  int numRegisters;
+
+  if (flag == 't') {
+    numRegisters = 10;
+  } else {
+    numRegisters = 8;
+  }
+
+  int registers[numRegisters];
+
+  while ((c = getchar()) != EOF) {
+    if (c == '#') {
+      while ((c = getchar()) != EOF && c != '\n') {
+      }
+    } else if (c == '$') {
+      c = getchar();
+      if (c == flag) {
+        c = getchar();
+        if (c >= '0' && c <= '7') {
+          registers[c - '0']++;
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < numRegisters; i++) {
+    printf("$%c%d: ", flag, i);
+    for (int j = 0; j < registers[i]; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
 }
 
-void printNumInstructions() {
-  printf("t\n");
-}
+void printNumInstructions() { printf("t\n"); }
